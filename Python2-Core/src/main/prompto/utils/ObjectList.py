ea95454d59da9ae7caabb817bd33f82b21caae28
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-06-29 00:19:09 +0200

    enum.symbols must return a list of symbols, enum.symbols.toString must return a list of symbol names

--- Python2-Core/src/main/prompto/utils/ObjectList.py
+++ Python2-Core/src/main/prompto/utils/ObjectList.py
@@ -2,14 +2,14 @@ from StringIO import StringIO
 
 class ObjectList ( list ):
 
-	def __init__(self, items = None):
-		super(ObjectList, self).__init__()
-		if items is not None:
-			self.extend(items)
-	
-	def __str__(self):
-		sb = StringIO()
-		for item in self:
-			sb.write(str(item))
-			sb.write(", " )
-		return sb.getvalue()[:-2]
+    def __init__(self, items = None):
+        super(ObjectList, self).__init__()
+        if items is not None:
+            self.extend(items)
+
+    def __str__(self):
+        sb = StringIO()
+        for item in self:
+            sb.write(str(item))
+            sb.write(", " )
+        return sb.getvalue()[:-2]
