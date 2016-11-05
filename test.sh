519a2eeca767e52fb056d9b3abab09caf0e50378
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-11-06 00:52:32 +0800

    check that Travis fails when tests fail

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python2-Core/src/generated
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python2-Core/src/main
-nosetests --exe Python2-Core/src/test/prompto/
+#nosetests --exe Python2-Core/src/test/prompto/
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python2-Runtime/src/main
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python2-Core/src/test
-nosetests --exe Python2-Runtime/src/test/
+nosetests --exe --with-xunit Python2-Runtime/src/test/
+python readXUnitTestResults.py nosetests.xml
