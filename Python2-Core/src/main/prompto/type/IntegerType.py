b2a1de91231a1f7c2c4e40f40a2a3c8e4da3b5fa
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-04-14 17:31:19 +0800

    support Integer.format

--- Python2-Core/src/main/prompto/type/IntegerType.py
+++ Python2-Core/src/main/prompto/type/IntegerType.py
@@ -1,7 +1,9 @@
 from numbers import Number
+
+from prompto.declaration.BuiltInMethodDeclaration import BuiltInMethodDeclaration
 from prompto.type.NativeType import NativeType
 from prompto.store.TypeFamily import TypeFamily
-
+from prompto.type.TextType import TextType
 
 
 class IntegerType(NativeType):
@@ -10,6 +12,7 @@ class IntegerType(NativeType):
 
     def __init__(self):
         super(IntegerType, self).__init__(TypeFamily.INTEGER)
+
 
     def isAssignableFrom(self, context, other):
         from prompto.type.DecimalType import DecimalType
@@ -25,6 +28,7 @@ class IntegerType(NativeType):
             return other
         return super(IntegerType, self).checkAdd(context, other, tryReverse)
 
+
     def checkSubstract(self, context, other):
         if isinstance(other, IntegerType):
             return self
@@ -32,6 +36,7 @@ class IntegerType(NativeType):
         if isinstance(other, DecimalType):
             return other
         return super(IntegerType, self).checkSubstract(context, other)
+
 
     def checkMultiply(self, context, other, tryReverse):
         from prompto.type.CharacterType import CharacterType
@@ -53,6 +58,7 @@ class IntegerType(NativeType):
             return other
         return super(IntegerType, self).checkMultiply(context, other, tryReverse)
 
+
     def checkDivide(self, context, other):
         from prompto.type.DecimalType import DecimalType
         if isinstance(other, IntegerType):
@@ -61,15 +67,18 @@ class IntegerType(NativeType):
             return other
         return super(IntegerType, self).checkDivide(context, other)
 
+
     def checkIntDivide(self, context, other):
         if isinstance(other, IntegerType):
             return IntegerType.instance
         return super(IntegerType, self).checkIntDivide(context, other)
 
+
     def checkModulo(self, context, other):
         if isinstance(other, IntegerType):
             return IntegerType.instance
         return super(IntegerType, self).checkModulo(context, other)
+
 
     def checkCompare(self, context, other):
         from prompto.type.DecimalType import DecimalType
@@ -80,11 +89,13 @@ class IntegerType(NativeType):
             return BooleanType.instance
         return super(IntegerType, self).checkCompare(context, other)
 
+
     def checkRange(self, context, other):
         if isinstance(other, IntegerType):
             from prompto.type.RangeType import RangeType
             return RangeType(self)
         return super(IntegerType, self).checkRange(context, other)
+
 
     def newRange(self, left, right):
         from prompto.value.Integer import Integer
@@ -93,8 +104,10 @@ class IntegerType(NativeType):
             return IntegerRange(left, right)
         return super(IntegerType, self).newRange(left, right)
 
+
     def sort(self, context, source, desc):
         return sorted(source, reverse=desc)
+
 
     def convertPythonValueToPromptoValue(self, context, value, returnType):
         if isinstance(value, Number):
@@ -103,5 +116,43 @@ class IntegerType(NativeType):
         else:
             return value  # TODO for now
 
+    def getMemberMethods(self, context, name):
+        if name == "format":
+            return [FormatMethodDeclaration()]
+        else:
+            return super(IntegerType, self).getMemberMethods(context, name)
 
 IntegerType.instance = IntegerType()
+
+class FormatMethodDeclaration(BuiltInMethodDeclaration):
+
+    def __init__(self):
+        from prompto.argument.CategoryArgument import CategoryArgument
+        from prompto.literal.TextLiteral import TextLiteral
+        FORMAT_ARGUMENT = CategoryArgument(TextType.instance, "format", TextLiteral('"format"'))
+        super(FormatMethodDeclaration, self).__init__("format", FORMAT_ARGUMENT)
+
+
+    def interpret(self, context):
+        from prompto.value.Text import Text
+        value = self.getValue(context).value
+        format = context.getValue("format").value
+        format = translateFormat(format)
+        text = format.format(value)
+        return Text(text)
+
+
+    def check(self, context):
+        from prompto.type.ListType import ListType
+        return ListType(TextType.instance)
+
+
+def translateFormat(f):
+    leadingZeros = 0
+    for c in xrange(0, len(f)):
+        if f[c] == '0':
+            leadingZeros = leadingZeros + 1
+    if leadingZeros > 0:
+        return "{0:0" + str(leadingZeros) + "d}"
+    else:
+        return "{0:d}"
