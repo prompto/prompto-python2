40e3a2ccbf929419e1c7f2468708194327a7f333
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-05-03 12:27:48 +0800

    fix issue where iterating on a cursor with a member expression would fail

--- Python2-Core/src/main/prompto/value/IterableValue.py
+++ Python2-Core/src/main/prompto/value/IterableValue.py
@@ -5,14 +5,14 @@ from prompto.runtime.Variable import Variable
 
 class IterableValue(BaseValue, IIterable):
 
-    def __init__(self, itemType, context, length, name, source, expression):
+    def __init__(self, context, name, itemType, source, length, expression):
         from prompto.type.IteratorType import IteratorType
         super(IterableValue, self).__init__(IteratorType(itemType))
-        self.itemType = itemType
         self.context = context
-        self.length = length
         self.name = name
+        self.itemType = itemType
         self.source = source
+        self.length = length
         self.expression = expression
 
     def getIterator(self, context):
