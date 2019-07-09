53ea6026814d7675f6cffeca07290840e8aad6e7
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-07-09 23:50:34 +0800

    fix failing tests

--- Python2-Core/src/main/prompto/expression/TypeExpression.py
+++ Python2-Core/src/main/prompto/expression/TypeExpression.py
@@ -4,20 +4,20 @@ from prompto.value.TypeValue import TypeValue
 
 class TypeExpression(IExpression):
 
-    def __init__(self, type):
-        self.type = type
+    def __init__(self, typ):
+        self.typ = typ
 
     def __str__(self):
-        return str(self.type)
+        return str(self.typ)
 
     def check(self, context):
-        return self.type
+        return self.typ
 
     def interpret(self, context):
-        return TypeValue(self.type)
+        return TypeValue(self.typ)
 
     def getMemberValue(self, context, name):
-        return self.type.getMemberValue(context, name)
+        return self.typ.getMemberValue(context, name)
 
     def toDialect(self, writer):
-        self.type.toDialect(writer)
+        self.typ.toDialect(writer)
