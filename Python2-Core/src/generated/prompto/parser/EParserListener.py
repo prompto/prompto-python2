c08230d44580be52760117fb9bb5917d90e11979
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-02-26 14:35:29 +0800

    fix antlr version

--- Python2-Core/src/generated/prompto/parser/EParserListener.py
+++ Python2-Core/src/generated/prompto/parser/EParserListener.py
@@ -1,4 +1,4 @@
-# Generated from EParser.g4 by ANTLR 4.8
+# Generated from EParser.g4 by ANTLR 4.9.1
 from antlr4 import *
 
 # This class defines a complete listener for a parse tree produced by EParser.
