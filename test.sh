04ff010962f3f095a51b2aa5c8c0189385d62d2c
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-01-20 02:36:30 +0100

    fix test script

--- test.sh
+++ test.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-ls -all
 export PYTHONPATH=$PYTHONPATH:$(pwd)/Python2-Core/src/generated
 export PYTHONPATH=$PYTHONPATH:$(pwd)/Python2-Core/src/main
-echo "PYTHONPATH" $PYTHONPATH
 nosetests --exe --with-xunit --xunit-file=testResults1.xml Python2-Core/src/test/prompto/
 export PYTHONPATH=$PYTHONPATH:$(pwd)/Python2-Runtime/src/main
 export PYTHONPATH=$PYTHONPATH:$(pwd)/Python2-Core/src/test
-echo "PYTHONPATH" $PYTHONPATH
 nosetests --exe --with-xunit --xunit-file=testResults2.xml Python2-Runtime/src/test/
 python readXUnitTestResults.py testResults1.xml testResults2.xml
