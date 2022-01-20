b34de09c137eeec96edd26e52f93f13913691db1
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-01-20 02:34:41 +0100

    fix test script

--- test.sh
+++ test.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 ls -all
-echo "PYTHONPATH" $PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:$CIRCLE_WORKING_DIRECTORY/Python2-Core/src/generated
-export PYTHONPATH=$PYTHONPATH:$CIRCLE_WORKING_DIRECTORY/Python2-Core/src/main
+export PYTHONPATH=$PYTHONPATH:$(pwd)/Python2-Core/src/generated
+export PYTHONPATH=$PYTHONPATH:$(pwd)/Python2-Core/src/main
 echo "PYTHONPATH" $PYTHONPATH
 nosetests --exe --with-xunit --xunit-file=testResults1.xml Python2-Core/src/test/prompto/
-echo "PYTHONPATH" $PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:$CIRCLE_WORKING_DIRECTORY/Python2-Runtime/src/main
-export PYTHONPATH=$PYTHONPATH:$CIRCLE_WORKING_DIRECTORY/Python2-Core/src/test
+export PYTHONPATH=$PYTHONPATH:$(pwd)/Python2-Runtime/src/main
+export PYTHONPATH=$PYTHONPATH:$(pwd)/Python2-Core/src/test
 echo "PYTHONPATH" $PYTHONPATH
 nosetests --exe --with-xunit --xunit-file=testResults2.xml Python2-Runtime/src/test/
 python readXUnitTestResults.py testResults1.xml testResults2.xml
